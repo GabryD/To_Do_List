215ae549ddc32021756692a36ba236db6fbd2777
N1351768 <gabry.dalcol2024@my.ntu.ac.uk>
2025-10-05 16:32:10 +0100

    Start Tasks class

---
+++ To_Do_List/To_Do_List.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+using namespace std;
+
+// Build a simple To - Do List application that runs in the terminal, lets you add, view, and delete tasks, and saves them to a file so they persist after closing.
+
+class Tasks {
+public:
+    string title;
+    int priority;
+
+};
+
+
+int main()
+{
+    cout << "To Do List" << endl;
+}
+
