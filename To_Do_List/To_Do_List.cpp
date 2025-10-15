6242eb774b27f1b6f6d8f55f125a9f9fbd76cf84
N1351768 <gabry.dalcol2024@my.ntu.ac.uk>
2025-10-15 23:32:04 +0100

    Class ability to read and write into a txt file need to fix user experience but will come later

--- To_Do_List/To_Do_List.cpp
+++ To_Do_List/To_Do_List.cpp
@@ -9,6 +9,10 @@ using namespace std;
 
 class Tasks {
 public:
+
+    Tasks() {
+    }
+
     Tasks(int taskId, string titleTask, string desc, bool comp)
     {
         id = taskId;
@@ -18,38 +22,107 @@ public:
 
     }; // initialising list
 
+    void taskCounter()
+    {
+        string taskCount;
+        ifstream taskFile("TasksList.txt", ios::in);
+        taskFile.seekg(-1, ios_base::end);
+        bool keepLooping = true;
+        while (keepLooping) {
+            char ch;
+            taskFile.get(ch);                            // Get current byte's data
+
+            if ((int)taskFile.tellg() <= 1) {             // If the data was at or before the 0th byte
+                taskFile.seekg(0);                       // The first line is the last line
+                keepLooping = false;                // So stop there
+            }
+            else if (ch == '\n') {                   // If the data was a newline
+                keepLooping = false;                // Stop at the current position.
+            }
+            else {                                  // If the data was neither a newline nor at the 0 byte
+                taskFile.seekg(-2, ios_base::cur);        // Move to the front of that data, then to the front of the data before it
+            }
+        }
+
+        string lastLine;
+        getline(taskFile, lastLine);                      // Read the current line
+        cout << "Result: " << lastLine << '\n';     // Display it
+
+        taskFile.close();
+
+    }
+
     void displayTask()
     {
         string myTasks;
-        ifstream taskFile("TasksList.txt");
+        ifstream taskFile("TasksList.txt",ios::in);
         while (getline(taskFile, myTasks)) 
         {
             cout << myTasks;
         }
+        taskFile.close();
     }
  
     void addTask()
     {
-        ofstream taskFile("TasksList.txt");
-        taskFile << "Test 123";
+        ofstream taskFile("TasksList.txt", ios::app);
+
+        taskFile  << id << " " << title << " " << description << " " << completed << "\n";
+
+
+        cout << "\nTask added";
         taskFile.close();
     }
 private:
-    int id;
-    string title;
-    string description;
-    bool completed;
+    int id = 0;
+    string title = "";
+    string description = "";
+    bool completed = false;
 };
 
 
 int main()
 {
+    bool exit = false;
     cout << "//////////////////////////" << endl;
     cout << "TO DO LIST" << endl;
-    Tasks task(1, "first", "working class", false);
-    task.addTask();
-    task.displayTask();
-    
+    Tasks count;
+    count.taskCounter();
+    while (exit == false) 
+    {
+        cout << "\n Add task: y/n: ";
+        char choice;
+        cin >> choice;
+        if (choice == 'y')
+        {
+            cout << "\nEnter Title of task: ";
+            string title;
+            cin >> title;
+            cout << "\nEnter Description of task: ";
+            string desc;
+            cin >> desc;
+            cout << "\nEnter Status of task (y/n): ";
+            char comp;
+            cin >> comp;
+            bool c;
+            if (comp == 'y')
+            {
+                c = true;
+            }
+            else
+            {
+                c = false;
+            }
+            Tasks task(1, title, desc, c);
+            task.addTask();
+
+        }
+        else
+        {
+            Tasks task(0, "", "", false);
+            task.displayTask();
+        }
+    }
 
 
 }
