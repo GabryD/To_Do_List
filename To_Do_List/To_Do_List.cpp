e5a5361d20d209b56e59b62f9837fd0c9c67e08e
N1351768 <gabry.dalcol2024@my.ntu.ac.uk>
2025-10-18 18:04:58 +0100

    added auto adding ID and a better layout to the output of the tasks

--- To_Do_List/To_Do_List.cpp
+++ To_Do_List/To_Do_List.cpp
@@ -1,8 +1,10 @@
-#include <iostream>
-#include <string>
-#include <iostream>
-#include <fstream>
 #include <ctime>
+#include <fstream>
+#include <iostream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <iomanip>
 using namespace std;
 
 // Build a simple To - Do List application that runs in the terminal, lets you add, view, and delete tasks, and saves them to a file so they persist after closing.
@@ -22,43 +24,60 @@ public:
 
     }; // initialising list
 
-    void taskCounter()
+    int taskCounter()
     {
-        string taskCount;
         ifstream taskFile("TasksList.txt", ios::in);
-        taskFile.seekg(-1, ios_base::end);
-        bool keepLooping = true;
-        while (keepLooping) {
-            char ch;
-            taskFile.get(ch);                            // Get current byte's data
-
-            if ((int)taskFile.tellg() <= 1) {             // If the data was at or before the 0th byte
-                taskFile.seekg(0);                       // The first line is the last line
-                keepLooping = false;                // So stop there
-            }
-            else if (ch == '\n') {                   // If the data was a newline
-                keepLooping = false;                // Stop at the current position.
-            }
-            else {                                  // If the data was neither a newline nor at the 0 byte
-                taskFile.seekg(-2, ios_base::cur);        // Move to the front of that data, then to the front of the data before it
+        if (!taskFile.is_open())
+        {
+            return 1;
+        }
+        string lastLine, line;
+        while (getline(taskFile, line))
+        {
+            if (!line.empty())
+            {
+                lastLine = line;
             }
         }
-
-        string lastLine;
-        getline(taskFile, lastLine);                      // Read the current line
-        cout << "Result: " << lastLine << '\n';     // Display it
-
         taskFile.close();
 
+        if (lastLine.empty())
+        {
+            return 1;
+        }
+
+        stringstream ss(lastLine);
+        int lastID;
+        ss >> lastID;
+
+        if (ss.fail())
+        {
+            return 1;
+        }
+
+        return lastID + 1;
     }
 
     void displayTask()
     {
         string myTasks;
         ifstream taskFile("TasksList.txt",ios::in);
-        while (getline(taskFile, myTasks)) 
+        int id;
+        string title, desc;
+        bool completed;
+        cout << "\n========== TO-DO LIST ==========\n";
+        cout << left << setw(5) << "ID"
+            << setw(20) << "TITLE"
+            << setw(30) << "DESCRIPTION"
+            << setw(10) << "STATUS" << "\n";
+        cout << string(65, '-') << "\n";
+
+
+        while (taskFile >> id >> title >> desc >> completed) 
         {
-            cout << myTasks;
+            cout << left << setw(5) << id
+                << setw(20) << title << setw(30) << desc << setw(10) << (completed ? "Done" : "Pending")
+                << "\n";
         }
         taskFile.close();
     }
@@ -67,7 +86,7 @@ public:
     {
         ofstream taskFile("TasksList.txt", ios::app);
 
-        taskFile  << id << " " << title << " " << description << " " << completed << "\n";
+        taskFile << id << " " << title << " " << description << " " << completed << "\n";
 
 
         cout << "\nTask added";
@@ -87,7 +106,6 @@ int main()
     cout << "//////////////////////////" << endl;
     cout << "TO DO LIST" << endl;
     Tasks count;
-    count.taskCounter();
     while (exit == false) 
     {
         cout << "\n Add task: y/n: ";
@@ -113,15 +131,20 @@ int main()
             {
                 c = false;
             }
-            Tasks task(1, title, desc, c);
+            Tasks task(count.taskCounter(), title, desc, c);
             task.addTask();
 
+        }
+        else if (choice == 'x')
+        {
+            exit = true;
         }
         else
         {
             Tasks task(0, "", "", false);
             task.displayTask();
         }
+        
     }
 
 
